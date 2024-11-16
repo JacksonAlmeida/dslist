b40a6140e4b8f24d226e163ee6caf9429520c26f
Jackson Rodrigues <jacksonnalmeida30@gmail.com>
2024-11-16 14:03:10 -0400

    :sparkles: feature: GET game by id

--- src/main/java/com/sunflower/dslist/resources/GameResources.java
+++ src/main/java/com/sunflower/dslist/resources/GameResources.java
@@ -1,10 +1,12 @@
 package com.sunflower.dslist.resources;
 
+import com.sunflower.dslist.dtos.GameDto;
 import com.sunflower.dslist.dtos.GameMinDto;
 import com.sunflower.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,12 @@ public class GameResources {
     public ResponseEntity<List<GameMinDto>> findAll() {
         List<GameMinDto> gameDtoList = gameService.findAll();
         return ResponseEntity.ok().body(gameDtoList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GameDto> findById( @PathVariable Long id) {
+        GameDto gameDto = gameService.findById(id);
+        return ResponseEntity.ok().body(gameDto);
     }
 
 }
--- src/main/java/com/sunflower/dslist/services/GameService.java
+++ src/main/java/com/sunflower/dslist/services/GameService.java
@@ -1,9 +1,12 @@
 package com.sunflower.dslist.services;
 
+import com.sunflower.dslist.dtos.GameDto;
 import com.sunflower.dslist.dtos.GameMinDto;
+import com.sunflower.dslist.entities.Game;
 import com.sunflower.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +16,18 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll() {
         return gameRepository.findAll().stream().map(GameMinDto::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id) {
+        Game game = gameRepository.findById(id).get();
+        if (game != null) {
+            return new GameDto(game);
+        }
+        return null;
     }
 
 }
